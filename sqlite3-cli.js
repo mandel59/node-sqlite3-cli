916bac6c334df748ea4e0545cfa8d499080d79cf
Ryusei Yamaguchi <mandel59@gmail.com>
2019-02-01 00:23:10 +0900

    create new connection for each time cli is start ed

--- sqlite3-cli.js
+++ sqlite3-cli.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 const util = require("util")
 const readline = require("readline")
 const sqlite3 = require("sqlite3")
@@ -21,16 +22,7 @@ class SQLite3CLI {
             crlfDelay: crlfDelay = Infinity,
             removeHistoryDuplicates: removeHistoryDuplicates,
         } = options
-        /** @type {Promise<sqlite3.Database>} */
-        this.db = new Promise((resolve, reject) => {
-            const db = new sqlite3.Database(dbfile, (err) => {
-                if (err) {
-                    reject(err)
-                    return
-                }
-                resolve(db)
-            })
-        })
+        this.dbfile = dbfile
         this.conin = conin
         this.conout = conout
         this.output = output
@@ -80,63 +72,84 @@ class SQLite3CLI {
             }
         }
         this.rl.addListener("SIGINT", handleSigInt)
-        const db = await this.db
-        while (true) {
-            const line = await this.nextLine()
-            if (line == null) {
-                break
-            }
-            if (line.trim() == "") {
-                continue
-            }
-            try {
-                /** @type {sqlite3.Statement} */
-                const stmt = await new Promise((resolve, reject) => {
-                    const stmt = db.prepare(line, (err) => {
-                        if (err) {
-                            reject(err)
-                            return
-                        }
-                        resolve(stmt)
-                    })
-                })
-                /** @type {() => Promise<Record<string, any> | null>} */
-                const getRow = () => {
-                    return new Promise((resolve, reject) => {
-                        interrupt = reject
-                        stmt.get((err, row) => {
+        /** @type {sqlite3.Database} */
+        const db = await new Promise((resolve, reject) => {
+            const db = new sqlite3.Database(this.dbfile, (err) => {
+                if (err) {
+                    reject(err)
+                    return
+                }
+                resolve(db)
+            })
+        })
+        try {
+            while (true) {
+                const line = await this.nextLine()
+                if (line == null) {
+                    break
+                }
+                if (line.trim() == "") {
+                    continue
+                }
+                try {
+                    /** @type {sqlite3.Statement} */
+                    const stmt = await new Promise((resolve, reject) => {
+                        const stmt = db.prepare(line, (err) => {
                             if (err) {
                                 reject(err)
                                 return
                             }
-                            resolve(row)
+                            resolve(stmt)
                         })
                     })
-                }
-                let nextRow = getRow()
-                /** @type {Record<string, any> | null} */
-                let row
-                while (row = await nextRow) {
-                    nextRow = getRow()
-                    if (!this.output.write(JSON.stringify(row) + "\n")) {
-                        await new Promise(resolve => {
-                            this.output.on("drain", resolve)
+                    /** @type {() => Promise<Record<string, any> | null>} */
+                    const getRow = () => {
+                        return new Promise((resolve, reject) => {
+                            interrupt = reject
+                            stmt.get((err, row) => {
+                                if (err) {
+                                    reject(err)
+                                    return
+                                }
+                                resolve(row)
+                            })
                         })
                     }
+                    let nextRow = getRow()
+                    /** @type {Record<string, any> | null} */
+                    let row
+                    while (row = await nextRow) {
+                        nextRow = getRow()
+                        if (!this.output.write(JSON.stringify(row) + "\n")) {
+                            await new Promise(resolve => {
+                                this.output.on("drain", resolve)
+                            })
+                        }
+                    }
+                } catch (err) {
+                    const ok = this.conout.write(
+                        util.inspect(err, {
+                            colors: this.rl.terminal
+                        }) + "\n")
+                    if (!ok) {
+                        await new Promise(resolve => {
+                            this.conout.on("drain", resolve)
+                        })
+                    }
+                } finally {
+                    interrupt = undefined
                 }
-            } catch (err) {
-                const ok = this.conout.write(
-                    util.inspect(err, {
-                        colors: this.rl.terminal
-                    }) + "\n")
-                if (!ok) {
-                    await new Promise(resolve => {
-                        this.conout.on("drain", resolve)
-                    })
-                }
-            } finally {
-                interrupt = undefined
             }
+        } finally {
+            new Promise((resolve, reject) => {
+                db.close((err) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve()
+                    }
+                })
+            })
         }
     }
 }
